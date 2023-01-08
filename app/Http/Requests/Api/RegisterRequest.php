6e621445dabbeab25b84f1487c9820c4c994c15d
Kuanysh <developerandroidkz@gmail.com>
2023-01-09 00:51:41 +0500

    sign up validation form

---
+++ app/Http/Requests/Api/RegisterRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'phone_number' => 'required|string|size:11',
+            'password' => 'required|string|min:8'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Введите имя'
+        ];
+    }
+}
