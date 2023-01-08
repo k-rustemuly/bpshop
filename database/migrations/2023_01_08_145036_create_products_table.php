7c5acbfa67608535dbfbc050172d41ecebf547ac
Kuanysh <developerandroidkz@gmail.com>
2023-01-09 00:53:25 +0500

    migration

---
+++ database/migrations/2023_01_08_145036_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('Название');
+            $table->string('description')->comment('Описание');
+            $table->string('slug');
+            $table->integer('category_id')->default(0)->index('category_id')->comment('ID категории второго/третьего уровня');
+            $table->decimal('price', 9, 2)->comment('Цена');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
