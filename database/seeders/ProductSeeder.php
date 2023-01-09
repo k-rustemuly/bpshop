51accdfb657b6cb805194584ac23e7c11c56a681
Kuanysh <developerandroidkz@gmail.com>
2023-01-09 22:25:24 +0500

    product seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+use Faker\Factory as Faker;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('ru_RU');
+        Product::truncate();
+        foreach (range(1, 200) as $index)  {
+            Product::create(
+                [
+                    'name' => $faker->sentence(3),
+                    'slug' => $faker->unique()->slug,
+                    'description' => $faker->paragraph(2),
+                    'category_id' => $faker->numberBetween(4, 28),
+                    'price' => $faker->numberBetween(1, 1250)*1000,
+                ]
+            );
+        }
+    }
+}
